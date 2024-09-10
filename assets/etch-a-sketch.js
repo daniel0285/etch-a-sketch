c3a8405298a8a14e347aca67ac47f0e0622fb0e6
Daniel Zabat <danielzabat01@gmail.noreply.github.com>
2024-09-10 23:24:33 +0800

    feat: Added buttons option, & rainbow pen function

--- assets/etch-a-sketch.js
+++ assets/etch-a-sketch.js
@@ -1,40 +1,64 @@
+const DEFAULT_VALUE = 16;
+
 const resizerLabel = document.getElementById("size-label");
 const resizer = document.getElementById("canvas-size");
 const colorPicker = document.getElementById("color-picker");
 const canvas = document.getElementById("canvas");
-const canvasTiles = document.querySelectorAll(".canvas-tiles");
 const deleteOption = document.getElementById("delete");
 
-console.log(deleteOption);
+console.log(deleteOption.checked);
 
 resizer.addEventListener("input", (event) => changeCanvas(event.target.value));
 canvas.addEventListener("mousedown", (event) => startDrawing(event));
 canvas.addEventListener("mouseup", (event) => stopDrawing(event));
-canvas.addEventListener("click", (event) => eraser(event));
+canvas.addEventListener("click", (event) => fillTile(event));
 deleteOption.addEventListener("click", () => console.log(deleteOption.checked));
 
 function fillTile(event) {
-  event.target.style.backgroundColor = penColor();
+  event.target.style.backgroundColor = rainbowColorPen();
 }
 
 function startDrawing(event) {
   const tile = document.querySelectorAll(".canvas-tiles");
-  event.preventDefault();
+  // event.preventDefault();
   tile.forEach((tile) => tile.addEventListener("mouseover", fillTile));
 }
 
 function stopDrawing(event) {
   const tile = document.querySelectorAll(".canvas-tiles");
-  event.preventDefault();
+  // event.preventDefault();
   tile.forEach((tile) => tile.removeEventListener("mouseover", fillTile));
 }
+
+function eraseTile(event) {
+  event.target.style.backgroundColor = "white";
+}
+
+function penColor() {
+  return colorPicker.value;
+}
+
+function rainbowColorPen(event) {
+  const rainbowColors = [
+    "#FF0000",
+    "#FF7F00",
+    "#FFFF00",
+    "#00FF00",
+    "#0000FF",
+    "#4B0082",
+    "#9400D3",
+  ];
+  let randomNumber = Math.floor(Math.random() * 6);
+  let randomColor = rainbowColors[randomNumber];
+  // event.target.style.backgroundColor = randomColor;
+  return randomColor;
+}
+console.log(rainbowColorPen);
 
 function defaultCanvas(defaultValue) {
   changeResizerLabel(defaultValue);
   createNewTiles(defaultValue);
 }
-
-defaultCanvas(16);
 
 function changeCanvas(sizeValue) {
   changeResizerLabel(sizeValue);
@@ -53,10 +77,16 @@ function changeResizerLabel(sizeValue) {
 }
 
 function createNewTiles(sizeValue) {
+  const fragment = document.createDocumentFragment();
   for (let i = 0; i < sizeValue * sizeValue; i++) {
     const newCanvasTile = document.createElement("div");
     newCanvasTile.classList.add("canvas-tiles");
     newCanvasTile.setAttribute("style", `flex: 1 1 calc(100% / ${sizeValue});`);
-    canvas.appendChild(newCanvasTile);
+    fragment.appendChild(newCanvasTile);
   }
+  canvas.appendChild(fragment);
 }
+
+// Function invokes
+
+defaultCanvas(DEFAULT_VALUE);
